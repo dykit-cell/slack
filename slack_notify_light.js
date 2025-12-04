b657f0b9c73c726494b073df43fe7d42e5741c8e
dykit-cell <kumaneko345@gmail.com>
2025-12-04 11:34:45 +0900

    Update slack_notify_light.js

--- slack_notify_light.js
+++ slack_notify_light.js
@@ -108,15 +108,15 @@ async function checkForUpdates() {
 
         // ページ内容が完全にロードされるまで待機
         try {
-            await page.waitForSelector('portal-listitem-text-inner', { timeout: 15000 }); // 15秒まで待機
+            await page.waitForSelector('p.portal-listitem-text-inner', { timeout: 15000 }); // 15秒まで待機
         } catch (error) {
-            console.error('エラー: `portal-listitem-text-inner` 要素が見つかりませんでした。ページ構造が変わった可能性があります。', error);
+            console.error('エラー: `p.portal-listitem-text-inner` 要素が見つかりませんでした。ページ構造が変わった可能性があります。', error);
             await sendSlackMessage('エラー: 回覧板の要素が見つかりませんでした。ウェブサイトの構造変更を確認してください。');
             return; // 処理を中断
         }
 
         // `tr.portal-item-expired` 内のリンクテキストを取得
-        const contentToCheck = await page.$eval('portal-listitem-text-inner a', element => element.textContent.trim());
+        const contentToCheck = await page.$eval('p.portal-listitem-text-inner a', element => element.textContent.trim());
         console.log('取得した回覧板コンテンツ:', contentToCheck);
 
         // コンテンツのハッシュを計算
